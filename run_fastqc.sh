fe3dbd01a76f7f7d0c2260630bf00d31d6a8f096
Alex Gorelick <agorelick@gmail.com>
2024-12-02 07:41:05 -0500

    Update run_fastqc.sh
    adding comments

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -9,7 +9,11 @@
 
 # load pre-installed FASTQC module on O2 
 module load fastqc/0.11.9
+
+# path to FASTQ files to be QC-ed
 fq_dir="/n/data1/hms/genetics/naxerova/lab/alex/azenta/30-1098079507_BR3_lpWGS/00_fastq"
+
+# path to output files from FASTQC
 fastqc_dir="/n/data1/hms/genetics/naxerova/lab/alex/example_patient/fastqc"
 
 # path to a directory where many large temporary files can safetly be written. On O2, this is best done in your "scratch" directory (see O2 wiki for instructions to create this directory).
